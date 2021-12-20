99efb56ad2461a1b72b86c1e2a312f0edfdb3cf1
ridaarif98 <ridaarif16@gmail.com>
2021-12-20 15:57:16 +0100

    Add links to sign-in and out

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/users/index"
+      expect(response).to have_http_status(:success)
+    end
   end
+
 end
