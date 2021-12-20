1e63e9cd3de0069889ee35c12c6272e591901534
ridaarif98 <ridaarif16@gmail.com>
2021-12-20 14:08:12 +0100

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_many :foods, dependent: :destroy
+    has_many :recipes, dependent: :destroy
+    has_many :inventories, dependent: :destroy
+
+    validates :name, presence: true, length: { minimum: 3 }
+end
