5b2f2d0a69f6f81d5244983108baf0bfef96cbe0
ridaarif98 <ridaarif16@gmail.com>
2021-12-21 12:04:24 +0100

    Fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,27 +1,26 @@
 class FoodsController < ApplicationController
-    def index
-        @foods = current_user.foods
-    end
+  def index
+    @foods = current_user.foods
+  end
 
-    def new
-        @user = current_user
-        @food = Food.new
-    end
+  def new
+    @user = current_user
+    @food = Food.new
+  end
 
-    def create
-        @food = Food.new(food_params)
-        if @food.save
-            flash[:notice] = 'Food created successfully.'
-            redirect_to foods_path
-        else
-            render :new
-        end
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      flash[:notice] = 'Food created successfully.'
+      redirect_to foods_path
+    else
+      render :new
     end
+  end
 
+  private
 
-    private
-    
-    def food_params
-        params.require(:food).permit(:name, :measurementUnit, :price, :user_id)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurementUnit, :price, :user_id)
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -3,5 +3,5 @@ class Food < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :measurementUnit, presence: true
-  validates :price, numericality: { greater_than: 0, less_than: 1000000 }
+  validates :price, numericality: { greater_than: 0, less_than: 1_000_000 }
 end
