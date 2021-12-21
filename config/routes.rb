4314ffadc4f2c85598d6b4e3cb96b476d33f4bd2
ridaarif98 <ridaarif16@gmail.com>
2021-12-21 11:09:10 +0100

    Update routes for foods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   get 'users/index'
-  resources :users, only: %i[index]
+  resources :users, only: %i[index] 
+  resources :foods, only: [:index, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
