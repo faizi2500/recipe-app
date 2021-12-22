f4042807927af1def716dd0c1ee3efc380ae2e14
ridaarif98 <ridaarif16@gmail.com>
2021-12-22 20:09:57 +0100

    Implement destror method for recipe-food

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,12 +1,14 @@
 class RecipeFoodsController < ApplicationController
     def new
        @recipe = Recipe.find(params[:recipe_id])
+       @recipe_food = @recipe.recipe_foods.new
     end
 
     def create
         @recipe = Recipe.find(params[:recipe_id])
-        @new_recipe_food = RecipeFood.new(quantity: params[:quantity], food_id: params[:food_id], recipe_id: @recipe.id)
-        if @new_recipe_food.save
+        @recipe_food = @recipe.recipe_foods.create(recipe_foods_params)
+        # @new_recipe_food = RecipeFood.new(quantity: params[:quantity], food_id: params[:food_id], recipe_id: @recipe.id)
+        if @recipe_food.save
             flash[:notice] = 'Food created successfully.'
             redirect_to @recipe
         else
@@ -14,4 +16,16 @@ class RecipeFoodsController < ApplicationController
         end
     end
 
+    def destroy
+        @recipe_food = RecipeFood.find(params[:id])
+        # authorize! :destroy, @recipe_food
+        @recipe_food.destroy 
+        flash[:success] = 'Comment was deleted successfully.'
+        redirect_to recipe_path(@recipe_food.recipe_id)
+    end
+
+    private
+    def recipe_foods_params
+        params.require(:recipe_food).permit(:quantity, :food_id)
+    end
 end
