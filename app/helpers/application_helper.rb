85bbdb83924d55db8fdb0a251a02ff0c09e01d24
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-22 17:34:43 +0500

    Fix rubocop errors for helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,6 @@
 module ApplicationHelper
-
   def check_owner(id)
     @inventory = Inventory.find(id)
-    if @inventory.user_id == current_user.id
-      @status = true
-    else
-      @status = false
-    end
+    @status = @inventory.user_id == current_user.id
   end
-  
 end
