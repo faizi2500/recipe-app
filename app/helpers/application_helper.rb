6d3901606fc5f3ec532fc3ebd57af15caa260ed3
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-22 17:27:01 +0500

    Add authorization for users to access inventories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def check_owner(id)
+    @inventory = Inventory.find(id)
+    if @inventory.user_id == current_user.id
+      @status = true
+    else
+      @status = false
+    end
+  end
+  
 end
