293de262d5eeb5a0d15d06f12321263148c5ffbf
ridaarif98 <ridaarif16@gmail.com>
2021-12-22 15:42:36 +0100

    Validate recipe-food model and define routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'users/index'
   resources :users, only: %i[index] 
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
   resources :inventories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
