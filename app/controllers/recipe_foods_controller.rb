3b63f0e95b38169c65ddd9921eff32173fc8164a
ridaarif98 <ridaarif16@gmail.com>
2021-12-23 11:10:40 +0100

    Create method to edit recipe food

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -16,15 +16,33 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    if @recipe_food.update(update_params)
+      flash[:success] = 'Recipe Food updated successfully.'
+    else
+      flash[:error] = 'Something went wrong'
+    end
+    redirect_to recipe_path(@recipe_food.recipe_id)
+  end
+  
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
     # authorize! :destroy, @recipe_food
     @recipe_food.destroy
-    flash[:success] = 'Comment was deleted successfully.'
+    flash[:success] = 'Recipe Food deleted successfully.'
     redirect_to recipe_path(@recipe_food.recipe_id)
   end
 
   private
+
+  def update_params
+    params.require(:recipe_food).permit(:quantity, :food_id)
+  end
 
   def recipe_foods_params
     params.require(:recipe_food).permit(:quantity, :food_id)
