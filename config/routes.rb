a838c291071d78fa99f3efd2d9944651ba891dd1
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-23 15:02:34 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     resources :inventory_foods
   end
   put 'recipes/:id/update', to: 'recipes#update', as: 'update'
+  post 'shopping_list/recipe_id=:recipe_id', to: 'recipes#generate_list', as: 'generate_shopping_list'
   get 'public_recipes', to: 'recipes#public', as: 'public'
   get 'shopping_list/recipe_id=:recipe_id&inventory_id=:inventory_id', to: 'recipes#generate', as: "shopping_list"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
