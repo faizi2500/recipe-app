e6c809f2432e4c9a098c262edcb8eb7945874ea9
faizi2500 <faizan.zahid2500@gmail.com>
2021-12-23 17:35:15 +0500

    Show shopping list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -45,16 +45,38 @@ class RecipesController < ApplicationController
     @recipes = Recipe.where(public: true)
   end
 
-  def generate_list 
+  def generate_list
     redirect_to shopping_list_path(params[:recipe_id], params[:inventory])
   end
 
   def generate
-    @recipe = Recipe.find_by(id: params[:recipe_id])
-    @inventory = Inventory.find_by(id: params[:inventory_id])
+    @quantity = []
     @foods = []
-    @foods << RecipeFood.where(recipe_id: params[:recipe_id])
-    @foods << InventoryFood.where(inventory_id: params[:inventory_id])
+    @recipe_food = RecipeFood.where(recipe_id: params[:recipe_id])
+    @inventory_food = InventoryFood.where(inventory_id: params[:inventory_id])
+    @recipe_food.each do |rf|
+      found = false
+      check_recipe_food = Food.find(rf.food_id)
+      @inventory_food.each do |each|
+        check_inventory_food = Food.find(each.food_id)
+        next unless check_inventory_food.name == check_recipe_food.name
+
+        found = true
+        required = rf.quantity - each.quantity
+        if required.positive?
+          @quantity << [required, check_inventory_food.price]
+          @foods << check_inventory_food.name
+        end
+      end
+      next if found
+
+      @quantity << [rf.quantity, check_recipe_food.price]
+      @foods << check_recipe_food.name
+    end
+    @total = 0
+    @quantity.each do |q|
+      @total += q[0] * q[1]
+    end
   end
 
   private
