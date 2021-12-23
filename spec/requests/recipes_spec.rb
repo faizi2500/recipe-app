b35d59eb8476f30f0f9ef56a8825c243c3cccbf0
medaminedev66 <md.amine.smahi@gmail.com>
2021-12-23 15:49:24 +0100

    Add tests to the recipes index view and recipes new view

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
-  describe 'GET /:index' do
-    it 'returns http success' do
-      get '/recipes/:index'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  include Devise::Test::IntegrationHelpers
 
-  describe 'GET /:show' do
-    it 'returns http success' do
-      get '/recipes/:show'
-      expect(response).to have_http_status(:success)
+  let(:user) { User.create(name: 'Amine', email: 'amine@mail.com', password: 'password') }
+  let(:recipe) { user.recipes.create(user_id: user.id, name: 'Wonderful cake', cookingTime: 5.5, preparationTime: 12.6, description: "Test the wondrful cake", public: true) }
+
+  describe 'GET /index' do
+    before do
+      sign_in user
+      get recipes_path
+    end
+
+    it 'should return response status correct (ok)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'respons to html' do
+      expect(response.content_type).to include 'text/html'
+    end
+
+    it 'should include correct placeholder' do
+      expect(response.body).to include('Add recipe')
     end
   end
 end
